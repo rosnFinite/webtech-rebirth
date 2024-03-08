11336eca9351db413ea4542be93a52d43defe788
Fabio Rose <fabiorose1811@gmail.com>
2024-03-08 13:10:36 +0100

    fixed spelling mistake

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-gradient-to-b from-black from-0% to-[#005194]">
-      <div className="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8 relaitve z-2">
+      <div className="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
         <nav className="flex flex-wrap justify-center -mx-5 -my-2">
           <div className="px-5 py-2">
             <a
